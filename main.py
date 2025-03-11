e56fac17d003a31fe56aff97f92f3b4ef86364d0
Dave-330 <tolum7@gmail.com>
2025-03-11 18:43:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,20 @@ def display_menu():
     print("2. Know if you can Drive")
     print("3. Know if you can run for president")
     print("4. Exit\n")
+    if choice == "1":
 
+        print("1, let’s find out if you can vote ")
+
+    elif choice == "2":
+
+        print(1"Great, you have to be 18 yers old to vote")
+
+    elif choice == "3":
+
+        print(1" oh ok!, thank you so much")
+
+        
 display_menu()
-
 choice = int(input("Enter the number of your choice: "))
 if choice == 4:
     print("Thanks for chatting with me. Goodbye!!")
